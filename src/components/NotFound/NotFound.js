498d63b592b5fe7474b45169d2ebb591155f6f86
Matheus Menegat <matheus.menegat@compasso.com.br>
2020-01-07 17:29:00 -0300

    Update page not found layout

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
 //Style
 import NotFoundImage from '../../assets/images/404.png';
+import Hr            from '../shared/Hr/Hr';
 import './NotFound.css'
 
 const NotFound = () => (
 
 	<section className="body-not-found">
-		<hr className="hr-404" />
 		<Image className="image-not-found" src={ NotFoundImage } fluid />
+		<p className='not-found'>404</p>
 		<h1 className="title-not-found">Page not found, <Link to="/" className="go-home">go home</Link></h1>
 	</section>
 );
